fa953575724d99dc2edd679b3783c83d49e41009
smartdevelopers <sathak318@gmail.com>
2019-06-30 11:29:07 +0300

    Update dynamic-field-component.ts

--- src/app/shared/components/dynamicform/components/dynamic-field-component.ts
+++ src/app/shared/components/dynamicform/components/dynamic-field-component.ts
@@ -12,23 +12,49 @@ import { map, startWith } from 'rxjs/operators';
 export class DynamicFieldComponent implements OnInit {
   @Input() field: any;
   @Input() group: FormGroup;
-  
+
   options: any;
   filteredOptions: any;
+
+  multiSelectSelected: any = [];
+
+  multiSelectfiltered: any=[];
+  lastFilter: string = '';
+
+
+
   myControl: FormControl = new FormControl();
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.group = this.createControl(this.field);
-    
-    if(this.field.type==="autocomplete"){
+
+    if (this.field.type === "autocomplete") {
       this.filteredOptions = this.group.controls[this.field.key].valueChanges.pipe(
         startWith(''),
         map(value => this._filter(value))
       );
     }
-    
+    else if (this.field.type === "multiautocomplete") {
+      this.multiSelectfiltered = this.group.controls[this.field.key].valueChanges.pipe(
+        startWith<string | any>(''),
+        map(value => typeof value === 'string' ? value : this.lastFilter),
+        map(filter => this.filter(filter))
+      );
+    }
+
   }
+  filter(filter: string): any {
+    this.lastFilter = filter;
+    if (filter) {
+      return this.field.options.filter(option => {
+        return option.displayText.toLowerCase().indexOf(filter.toLowerCase()) >= 0;
+      })
+    } else {
+      return this.field.options.slice();
+    }
+  }
+
   private _filter(value: any) {
     const filterValue = value;
 
@@ -39,9 +65,9 @@ export class DynamicFieldComponent implements OnInit {
     if (field.type === "button") return;
     const control = this.fb.control(
       field.value,
-      field.required? [
+      field.required ? [
         Validators.required
-      ]:[]
+      ] : []
     );
 
     this.group.addControl(field.key, control);
@@ -52,9 +78,42 @@ export class DynamicFieldComponent implements OnInit {
   displayFn(option): string {
     return option;
   }
+
+  displaymultiFn(value): string | undefined {
+    let displayValue: string;
+    if (Array.isArray(value)) {
+      value.forEach((arr, index) => {
+        if (index === 0) {
+          displayValue = arr.displayText;
+        } else {
+          displayValue += ', ' + arr.displayText;
+        }
+      });
+    } else {
+      displayValue = value;
+    }
+    return displayValue;
+  }
+
   selectedclient(event) {
     console.log(event.option.value);
 
+  }
+  optionClicked(event: Event, arr: any) {
+    event.stopPropagation();
+    this.toggleSelection(arr);
+  }
+
+  toggleSelection(arr: any) {
+    arr.selected = !arr.selected;
+    if (arr.selected) {
+      this.multiSelectSelected.push(arr);
+    } else {
+      const i = this.multiSelectSelected.findIndex(value => value.Id === arr.Id);
+      this.multiSelectSelected.splice(i, 1);
+    }
+
+    this.group.controls[this.field.key].setValue(this.multiSelectSelected);
   }
 
 }
